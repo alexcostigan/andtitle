41a3133857ac519c12b8e0215e952d0f01fa6322
Beenish Butt <beenishbutt@Beenishs-MBP.broadband>
2021-03-09 16:16:04 +0000

    push to master

--- src/Components/Textbox.jsx
+++ src/Components/Textbox.jsx
@@ -3,11 +3,22 @@ import React from 'react'
 class Textbox extends React.Component {
     render(){
         return(
-            <div>
-                <input 
-                    placeholder="Please Enter Your AND Title"
-                    required />
-            </div>
+            <div class="card" style={{width: "18rem"}}>
+    <form>
+  <div class="mb-3">
+    
+    <input type="text" placeholder ="enter your name" class="form-control" id="name" />
+   
+  </div>
+  <div class="mb-3">
+    
+    <input type="text" placeholder ="enter your AND title" class="form-control" id="name" />
+   
+  </div>
+  
+  <input type="submit" class="btn btn-primary">Submit</input>
+</form>
+</div>
         )
     }
 }
